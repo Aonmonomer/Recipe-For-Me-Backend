7e83df333426891820a8172be860e7804b055402
jlangner87 <josh.langner@gmail.com>
2022-08-12 13:24:59 -0500

    recipe update operation test on insomnia successful

--- controllers/RecipesController.js
+++ controllers/RecipesController.js
@@ -5,6 +5,16 @@ const FindAllRecipes = async (req, res) => {
   res.send(allRecipes)
 }
 
+const FindByCategory = async (req, res) => {
+  let categoryId = parseInt(req.params.user_Id)
+  let recipeByCategory = await Recipe.findAll({
+    where: {
+      categoryId: categoryId
+    }
+  })
+  res.send(recipeByCategory)
+}
+
 const CreateRecipe = async (req, res) => {
   let newRecipeBody = {
     ...req.body
@@ -20,8 +30,20 @@ const DeleteRecipe = async (req, res) => {
   })
   res.send({ message: `You deleted the recipe with id ${recipeId}` })
 }
+
+const UpdateRecipe = async (req, res) => {
+  let recipeId = parseInt(req.params.recipe_id)
+  let updatedRecipe = await Recipe.update(req.body, {
+    where: { id: recipeId },
+    returning: true
+  })
+  res.send(updatedRecipe)
+}
+
 module.exports = {
   FindAllRecipes,
+  FindByCategory,
   CreateRecipe,
-  DeleteRecipe
+  DeleteRecipe,
+  UpdateRecipe
 }
--- routes/RecipesRouter.js
+++ routes/RecipesRouter.js
@@ -2,6 +2,8 @@ const Router = require('express').Router()
 const controller = require('../controllers/RecipesController')
 
 Router.get('/all', controller.FindAllRecipes)
+Router.get('/:category_id', controller.FindByCategory)
 Router.post('/create', controller.CreateRecipe)
 Router.delete('/:recipe_id', controller.DeleteRecipe)
+Router.put('/:recipe_id', controller.UpdateRecipe)
 module.exports = Router
