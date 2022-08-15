6f5675157f403e277684875e935b900c1210073e
jlangner87 <josh.langner@gmail.com>
2022-08-15 12:54:24 -0500

    fixed bug with GET call on FindOneRecipe

--- routes/RecipesRouter.js
+++ routes/RecipesRouter.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/RecipesController')
 
 Router.get('/all', controller.FindAllRecipes)
 Router.get('/:category_id', controller.FindByCategory)
-Router.get('/recipe_id', controller.FindOneRecipe)
+Router.get('/view/recipe_id', controller.FindOneRecipe)
 Router.post('/create', controller.CreateRecipe)
 Router.delete('/:recipe_id', controller.DeleteRecipe)
 Router.put('/:recipe_id', controller.UpdateRecipe)
