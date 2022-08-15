aa6c4253e17c29574dfc01758c2752b1007e0e0a
Seng Kit Mun <cellkit11@gmail.com>
2022-08-15 11:18:12 -0400

    Added get all users details

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,6 +1,7 @@
 const Router = require('express').Router()
 const controller = require('../controllers/UsersController')
 
+Router.get('/all', controller.GetAllUserDetails)
 Router.get('/view/:user_id', controller.GetUserDetails)
 Router.post('/create', controller.CreateUser)
 Router.put('/:user_id', controller.UpdateUser)
