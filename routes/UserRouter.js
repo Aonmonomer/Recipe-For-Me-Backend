2f73f21fd4fc8c2b689bd64d77129a89905d0019
Seng Kit Mun <cellkit11@gmail.com>
2022-08-17 12:14:09 -0400

    relaunch heroku

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -5,7 +5,6 @@ Router.get('/all', controller.GetAllUserDetails)
 Router.get('/view/:user_id', controller.GetUserDetails)
 Router.post('/create', controller.CreateUser)
 Router.put('/:user_id', controller.UpdateUser)
-
 Router.delete('/:user_id', controller.DeleteUser)
 
 module.exports = Router
