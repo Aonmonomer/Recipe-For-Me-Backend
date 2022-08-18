46d990db649e44dbfe429a569e83c0625ce14da7
Seng Kit Mun <cellkit11@gmail.com>
2022-08-17 20:03:30 -0400

    try

--- middleware/index.js
+++ middleware/index.js
@@ -2,6 +2,9 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
+// const SALT_ROUNDS = 12
+// const APP_SECRET = 'supersecretkey'
+
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
 const APP_SECRET = process.env.APP_SECRET
 
